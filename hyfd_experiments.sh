e5ce0e1740dd80814212a6e4ff21690f25e34c81
Victor Codocedo <victor.codocedo@gmail.com>
2019-10-24 15:09:50 +0200

    added experiments batch

---
+++ hyfd_experiments.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+pypy3 db_dgfd.py data/diagnostics.csv
+pypy3 db_dgfd.py data/forestfires.csv
+pypy3 db_dgfd.py data/abalone.csv
+pypy3 db_dgfd.py data/pglw00.original.csv
+pypy3 db_dgfd.py data/servo.original.csv
+pypy3 db_dgfd.py data/adult.data.csv
+pypy3 db_dgfd.py data/caulkins.csv
+pypy3 db_dgfd.py data/hughes.original.csv
+pypy3 db_dgfd.py data/cmc.data.csv
+pypy3 db_dgfd.py data/credit.data.csv
+pypy3 db_dgfd.py data/ncvoter_1001r_19c.csv
+pypy3 db_dgfd.py data/hepatitis.csv
+pypy3 db_dgfd.py data/horse.csv -s ';'
+pypy3 db_dgfd.py data/mushroom.csv
+pypy3 db_dgfd.py data/flight_1k.csv -s ';'
+pypy3 db_dgfd.py data/fd-reduced-30.csv

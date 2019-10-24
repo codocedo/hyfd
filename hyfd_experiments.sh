88f8bf2f922c88f714285e2d87b0079f1595a6e9
Victor Codocedo <victor.codocedo@gmail.com>
2019-10-24 15:12:01 +0200

    fixed experiments batch

--- hyfd_experiments.sh
+++ hyfd_experiments.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-pypy3 db_dgfd.py data/diagnostics.csv
-pypy3 db_dgfd.py data/forestfires.csv
-pypy3 db_dgfd.py data/abalone.csv
-pypy3 db_dgfd.py data/pglw00.original.csv
-pypy3 db_dgfd.py data/servo.original.csv
-pypy3 db_dgfd.py data/adult.data.csv
-pypy3 db_dgfd.py data/caulkins.csv
-pypy3 db_dgfd.py data/hughes.original.csv
-pypy3 db_dgfd.py data/cmc.data.csv
-pypy3 db_dgfd.py data/credit.data.csv
-pypy3 db_dgfd.py data/ncvoter_1001r_19c.csv
-pypy3 db_dgfd.py data/hepatitis.csv
-pypy3 db_dgfd.py data/horse.csv -s ';'
-pypy3 db_dgfd.py data/mushroom.csv
-pypy3 db_dgfd.py data/flight_1k.csv -s ';'
-pypy3 db_dgfd.py data/fd-reduced-30.csv
+pypy3 hyfd.py -m -el 0.001 data/diagnostics.csv
+pypy3 hyfd.py -m -el 0.001 data/forestfires.csv
+pypy3 hyfd.py -m -el 0.001 data/abalone.csv
+pypy3 hyfd.py -m -el 0.001 data/pglw00.original.csv
+pypy3 hyfd.py -m -el 0.001 data/servo.original.csv
+pypy3 hyfd.py -m -el 0.001 data/adult.data.csv
+pypy3 hyfd.py -m -el 0.001 data/caulkins.csv
+pypy3 hyfd.py -m -el 0.001 data/hughes.original.csv
+pypy3 hyfd.py -m -el 0.001 data/cmc.data.csv
+pypy3 hyfd.py -m -el 0.001 data/credit.data.csv
+pypy3 hyfd.py -m -el 0.001 data/ncvoter_1001r_19c.csv
+pypy3 hyfd.py -m -el 0.001 data/hepatitis.csv
+pypy3 hyfd.py -m -el 0.001 data/horse.csv -s ';'
+pypy3 hyfd.py -m -el 0.001 data/mushroom.csv
+pypy3 hyfd.py -m -el 0.001 data/flight_1k.csv -s ';'
+pypy3 hyfd.py -m -el 0.001 data/fd-reduced-30.csv
